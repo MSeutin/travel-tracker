3d111296542776e1af2dff1ab0a659070e7383ec
Anudari Gereltod <anuu1799@gmail.com>
2023-08-12 18:41:35 -0700

    worked on account page beautify

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -11,14 +11,11 @@ export default function Account() {
         }
       };
   return (
-    <div className='w-[300px] m-auto'>
-      <h1 className='text-center text-2xl font-bold pt-12'>Account</h1>
-      <div>
-        <p>Welcome, {user?.displayName}</p>
-      </div>
-      <button onClick={handleLogOut} className='border py-2 px-5 mt-10'>
-        Logout
-      </button>
+    <div className='flex justify-center flex-col items-center'>
+      <h1 className='text-center text-2xl font-bold pt-12 mb-10'>
+        Welcome, {user?.displayName}
+      </h1>
+      
       <ListContainer />
     </div>
   );
