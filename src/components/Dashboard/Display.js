b3e5947bcd82bbc2670b0f2e29e43110d5d0e2fe
Michael Seutin <frenchmike@gmail.com>
2023-08-14 18:38:37 -0700

    finished time api

--- src/components/Dashboard/Display.js
+++ src/components/Dashboard/Display.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
+import { faClock } from "@fortawesome/free-solid-svg-icons"; // Import the clock icon
 
 export default function Display() {
   const cities = [
-    { name: "Tokyo", continent: "Asia" },
-    { name: "Cairo", continent: "Africa" },
-    { name: "New York", continent: "North America" },
-    { name: "Sao Paulo", continent: "South America" },
-    { name: "London", continent: "Europe" },
-    { name: "Sydney", continent: "Australia" },
+    { name: "Tokyo", continent: "Asia", timezone: "Asia/Tokyo" },
+    { name: "Cairo", continent: "Africa", timezone: "Africa/Cairo" },
+    { name: "New York", continent: "North America", timezone: "America/New_York" },
+    { name: "Sao Paulo", continent: "South America", timezone: "America/Sao_Paulo" },
+    { name: "London", continent: "Europe", timezone: "Europe/London" },
+    { name: "Sydney", continent: "Australia", timezone: "Australia/Sydney" },
   ];
 
   // You can define your API calls for currency exchange, time, and weather here
@@ -25,7 +27,12 @@ useEffect(() => {
           const response = await axios.get(
             `http://worldtimeapi.org/api/timezone/${city.timezone}`
           );
-          return { city: city.name, time: response.data.datetime };
+          const datetime = response.data.datetime;
+          const [datePart, timePart] = datetime.split("T");
+          const [year, month, day] = datePart.split("-").map(Number);
+          const [time, _] = timePart.split(".");
+          const [hours, minutes, seconds] = time.split(":").map(Number);
+          return { city: city.name, time: `${hours}:${minutes}` };
         } catch (error) {
           console.error(`Error fetching time for ${city.name}:`, error);
           return { city: city.name, time: "Time not available" };
@@ -55,13 +62,28 @@ useEffect(() => {
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
         {cities.map((city) => {
           const cityTime = cityTimes.find((ct) => ct.city === city.name);
+          console.log(`City time for ${city.name}:`, cityTime);
           return (
             <div
               key={city.name}
               className="border p-4 rounded-md flex flex-col items-center hover:bg-zinc-500 hover:text-white justify-center text-center"
             >
               <h3 className="text-xl font-semibold">{city.name}</h3>
-              <p>{cityTime ? cityTime.time : "Loading..."}</p>
+              <p>
+                {cityTime ? (
+                  <>
+                    <span className=" text-blue-500">
+                      {cityTime.time.split(":")[0]}
+                    </span>
+                    <span className="text-gray-400 animate-pulse">:</span>
+                    <span className=" text-red-500">
+                      {cityTime.time.split(":")[1]}
+                    </span>
+                  </>
+                ) : (
+                  "Loading..."
+                )}
+              </p>
               {/* Display currency exchange and weather information here */}
             </div>
           );
